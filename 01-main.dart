d8fe7a10bca6b558634734702f2d2cecdfb2e8e4
Julian Riveros <juliancamilorifo@ufps.edu.co>
2023-08-09 10:06:52 -0500

    Inicio con dart

---
+++ 01-main.dart
@@ -0,0 +1,15 @@
+
+/// Documentación del main
+void main() {
+  
+  // Comentarios
+  
+  /*
+   *  Esto es un comentario multilinea
+   *  aaaaaaaaa
+   */
+  
+  
+  print('Hola Mundo');
+  
+}
---
+++ 02_interpolacion_strings.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+//   var myName = 'Julian';
+//   late final myName;
+//   String myName = 'Julian';
+  const myName = 'Julian';
+    
+  
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }');
+}
